25d9941eb740b3672991ae41f792f16671dc453b
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2025-05-04 21:15:37 +0200

    Llamadas a archivos sql

---
+++ database/init_script.sql
@@ -0,0 +1,16 @@
+-- Script de inicialización de la base de datos
+
+-- Crear tipos
+@create_types.sql
+
+-- Crear tablas
+@create_tables.sql
+
+-- Crear disparadores
+@create_triggers.sql
+
+-- Crear paquetes (procedimientos y funciones)
+@create_package.sql
+
+-- Insertar datos iniciales
+@insert_data.sql
