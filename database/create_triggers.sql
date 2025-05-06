35987fb846d6308e5c1bacb886ac15cc3a86724f
luciacopano <127422970+luciacopano@users.noreply.github.com>
2025-05-06 18:43:31 +0200

    triggers añadidos

--- database/create_triggers.sql
+++ database/create_triggers.sql
@@ -27,3 +27,44 @@ END;
 
 
 /
+
+--Trigger para no borrar una actividad si hay una visita o exposicion activa
+CREATE OR REPLACE TRIGGER trigger_borrar_actividad
+BEFORE DELETE ON ACTIVIDADES FOR EACH ROW
+
+DECLARE
+    v_visitas NUMBER; --GUARDARÁN EL NÚMERO DE VISITAS Y EXPOSICIONES
+    v_exposicion NUMBER;
+    
+BEGIN
+    SELECT COUNT(*) INTO v_visitas FROM VISITA WHERE cod_actividad = :OLD.cod_actividad;
+
+    IF v_visitas > 0 THEN
+        RAISE_APPLICATION_ERROR(-20008,"No se puede borrar la actividad porque hay una visita activa");
+    END IF;
+
+    SELECT COUNT(*) INTO v_exposicion FROM EXPOSICION WHERE cod_actividad = :OLD.cod_actividad;
+
+    IF v_exposicion > 0 THEN
+        RAISE_APPLICATION_ERROR(-20009,"No se puede borrar la actividad porque hay una exposicion activa");
+    END IF;
+END;
+/
+
+--Trigger para evitar que haya obras anteriores al nacimiento de su autor
+CREATE OR REPLACE TRIGGER trigger_update_obras
+AFTER UPDATE ON OBRA_DE_ARTE FOR EACH ROW
+
+DECLARE 
+    v_fecha DATE;
+
+BEGIN
+    SELECT fecha_nacimiento INTO v_fecha FROM AUTORES WHERE cod_autor = :NEW.cod_autor;
+
+    IF :NEW.fecha_creacion IS NOT NULL AND v_fecha IS NOT NULL THEN
+        IF :NEW.fecha_creacion < v_fecha THEN
+            RAISE_APPLICATION_ERROR(-20003, "La fecha de creacion de la obra no puede ser anterior a la de nacimiento del autor");
+        END IF;
+    END IF;
+END;
+/
