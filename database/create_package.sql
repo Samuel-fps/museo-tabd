2da855b26d699a09368eae2d16bdd4db424b6451
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2025-05-11 19:32:03 +0200

    Correecion de funciones y mejora en limpieza de código

--- database/create_package.sql
+++ database/create_package.sql
@@ -1,8 +1,11 @@
 
 -- Paquete ventas
 CREATE OR REPLACE PACKAGE pkg_ventas IS
+
     PROCEDURE registrar_venta (v_cliente IN NUMBER, v_empleado IN NUMBER, v_entrada IN NUMBER);
-END create_package;
+    FUNCTION entradas_vendidas_mes(p_mes IN NUMBER, p_anio IN NUMBER) RETURN NUMBER;
+
+END pkg_ventas;
 
 -- Cuerpo del paquete ventas
 CREATE OR REPLACE PACKAGE BODY pkg_ventas AS
@@ -24,45 +27,91 @@ CREATE OR REPLACE PACKAGE BODY pkg_ventas AS
 
       DBMS_OUTPUT.PUT_LINE('Venta registrada, ID venta: ' || v_venta_id);
   END;
+
+  -- Devulve el número de entradas vendidas en un mes y año específicos
+  CREATE OR REPLACE FUNCTION entradas_vendidas_mes(p_mes IN NUMBER, p_anio IN NUMBER) 
+  RETURN NUMBER IS
+    v_num_entradas NUMBER;
+  BEGIN
+    SELECT COUNT(*) INTO v_num_entradas FROM ENTRADAS
+    WHERE EXTRACT(MONTH FROM fecha) = p_mes
+    AND EXTRACT(YEAR FROM fecha) = p_anio;
+        
+    RETURN v_num_entradas;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RETURN 0;
+  END entradas_vendidas_mes;
+
 END pkg_ventas;
 
---Paquete Autor
+-- Paquete Autor
 CREATE OR REPLACE PACKAGE pkg_autor IS
+
   FUNCTION total_obras_autor(v_autor IN NUMBER) RETURN NUMBER;
-END create_package;
+
+END pkg_autor;
 
 -- Cuerpo del paquete Autor
 CREATE OR REPLACE PACKAGE BODY pkg_autor AS
+
   FUNCTION total_obras_autor(v_autor IN NUMBER) RETURN NUMBER IS v_total_obras NUMBER;
   BEGIN
       SELECT COUNT(*) INTO v_total_obras FROM Autor a JOIN OBRA_DE_ARTE o ON a.cod_autor = o.cod_autor WHERE v_autor = a.cod_autor;
 
       RETURN v_total_obras;
-END;
+  END total_obras_autor;
+
 END pkg_autor;
 
 
---Paquete Visitas
+-- Paquete Visitas
 CREATE OR REPLACE PACKAGE pkg_visitas IS
-  FUNCTION cantidad_visitas_por_mes(v_mes IN NUMBER, v_anno IN NUMBER) RETURN NUMBER;
-END create_package;
+
+  FUNCTION cantidad_visitas_mes(v_mes IN NUMBER, v_anno IN NUMBER) RETURN NUMBER;
+  FUNCTION cantidad_plazas_disponibles(cod_visita IN NUMBER) RETURN NUMBER;
+
+END pkg_visitas;
 
 -- Cuerpo del paquete Visitas
 CREATE OR REPLACE PACKAGE BODY pkg_visitas AS
-FUNCTION cantidad_visitas_por_mes(v_mes IN NUMBER, v_anno IN NUMBER) RETURN NUMBER IS v_total_visitas NUMBER;
-BEGIN
-    SELECT COUNT(*) INTO v_total_visitas FROM VISITA v JOIN ACTIVIDADES a ON v.cod_actividad = a.cod_actividad WHERE v.tipo = 'Guiada'
-    AND EXTRACT (MONTH FROM a.fecha_inicio) = v_mes;
-    AND EXTRACT (YEAR FROM a.fecha_inicio) = v_anno;
 
-    RETURN v_total_visitas;
-END;
+  -- Devuelve el número de plazas disponibles para una visita
+  CREATE OR REPLACE FUNCTION cantidad_plazas_disponibles(cod_visita IN NUMBER) 
+  RETURN NUMBER 
+  IS
+      v_cupo_maximo    NUMBER;
+      entradas_reservadas NUMBER;
+      plazas_disponibles NUMBER;
+  BEGIN
+      -- Obtener el cupo máximo de la visita
+      SELECT cupo_maximo INTO v_cupo_maximo
+      FROM VISITAS WHERE cod_visita = cod_visita;
+
+      -- Calcular el número de entradas reservadas para la visita
+      SELECT COUNT(*) INTO entradas_reservadas
+      FROM ENTRADAS WHERE cod_visita = cod_visita;
+
+      -- Calcular las plazas disponibles
+      plazas_disponibles := cupo_maximo - entradas_reservadas;
+
+      RETURN plazas_disponibles;
+  EXCEPTION
+      WHEN NO_DATA_FOUND THEN
+          RETURN -1; -- No se encontró la visita
+      WHEN OTHERS THEN
+          RETURN NULL;
+  END obtener_plazas_disponibles;
+  /
+
 END pkg_visitas;
 
 -- Paquete Entradas
 CREATE OR REPLACE PACKAGE pkg_entradas IS
+
   PROCEDURE total_entradas_vendidas_por_fecha(v_fecha IN DATE, v_total OUT NUMBER);
-END create_package;
+
+END pkg_entradas;
 
 -- Cuerpo del paquete Entradas
 CREATE OR REPLACE PACKAGE BODY pkg_entradas AS
@@ -84,21 +133,24 @@ CREATE OR REPLACE PACKAGE BODY pkg_entradas AS
   END;
   /
 
-
 END pkg_entradas;
 
 --Paquete Contrato
 CREATE OR REPLACE PACKAGE pkg_contrato IS
-PROCEDURE actualizar_estado_contratos;
-END create_package;
 
--- Cuerpo del paquete Contrato
+  PROCEDURE actualizar_estado_contratos;
+
+END pkg_contrato;
+
+  -- Cuerpo del paquete Contrato
 CREATE OR REPLACE PACKAGE BODY pkg_contrato AS
-PROCEDURE actualizar_estado_contratos AS
-    BEGIN
-        UPDATE CONTRATO
-        SET estado = 'Inactivo';
-        WHERE fecha_fin IS NOT NULL;
-    END;
-END;
+
+  PROCEDURE actualizar_estado_contratos AS
+      BEGIN
+          UPDATE CONTRATO
+          SET estado = 'Inactivo';
+          WHERE fecha_fin IS NOT NULL;
+      END;
+  END actualizar_estado_contratos;
+
 END pkg_contrato;
