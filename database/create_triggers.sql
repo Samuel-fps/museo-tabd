91c303219d20470f0d47b45a007ab0d1cf615e75
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2025-05-12 19:49:33 +0200

    Correcciones triggers

--- database/create_triggers.sql
+++ database/create_triggers.sql
@@ -1,36 +1,73 @@
 -- Archivo donde se contienen los triggers de la base de datos
 
--- GENERAL
-
--- Trigger para evitar eliminar tablas por error
-CREATE OR REPLACE TRIGGER no_drop_table
-BEFORE DROP ON DATABASE
+-- Eliminar los triggers si ya existen antes de crearlos para evitar errores
 BEGIN
-    RAISE_APPLICATION_ERROR(-20000, 'No está permitido eliminar tablas en esta base de datos.');
+    FOR trigger_record IN (SELECT trigger_name FROM user_triggers WHERE trigger_name IN ('TRG_BORRAR_ACTIVIDAD', 'NO_BORRAR_AUTOR_CON_OBRAS', 'TRG_UPDATE_NUM_OBRAS', 'TRG_INSERTAR_NUM_OBRAS', 'TRG_ACTUALIZAR_NUM_OBRAS', 'TRG_ELIMINAR_OBRA')) LOOP
+        EXECUTE IMMEDIATE 'DROP TRIGGER ' || trigger_record.trigger_name;
+    END LOOP;
 END;
 /
+
+-- Ahora, puedes volver a crear los triggers aquí
+
+-- GENERAL
+
+-- ACTIVIDADES
+
+-- Trigger para no borrar una actividad si hay una visita o exposición activa
+CREATE OR REPLACE TRIGGER trg_borrar_actividad
+BEFORE DELETE ON VISITAS FOR EACH ROW
+DECLARE
+    v_visitas NUMBER; --GUARDARÁN EL NÚMERO DE VISITAS Y EXPOSICIONES
+    v_exposicion NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_visitas FROM VISITAS WHERE cod_visita = :OLD.cod_visita;
+
+    IF v_visitas > 0 THEN
+        RAISE_APPLICATION_ERROR(-20008, 'No se puede borrar la actividad porque hay una visita activa');
+    END IF;
+
+END;
+/
+
+-- AUTORES
+
+-- Trigger para evitar eliminar autores que tengan obras registradas
+CREATE OR REPLACE TRIGGER no_borrar_autor_con_obras
+BEFORE DELETE ON AUTORES
+FOR EACH ROW
+DECLARE
+    v_count NUMBER;
+BEGIN
+    SELECT COUNT(*) INTO v_count -- Contar el número de obras asociadas al autor
+    FROM OBRAS
+    WHERE cod_autor = :OLD.cod_autor;
+
+    IF v_count > 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'No se puede eliminar el autor porque tiene obras registradas.');
+    END IF;
+END;
+/
+
+-- OBRAS
+
+
+-- GENERAL
 
 -- ACTIVIDADES
 
 -- Trigger para no borrar una actividad si hay una visita o exposicion activa
-CREATE OR REPLACE TRIGGER trg_borrar_actividad
-BEFORE DELETE ON ACTIVIDADES FOR EACH ROW
+CREATE OR REPLACE TRIGGER trg_borrar_visita
+BEFORE DELETE ON VISITAS FOR EACH ROW
 
 DECLARE
-    v_visitas NUMBER; --GUARDARÁN EL NÚMERO DE VISITAS Y EXPOSICIONES
-    v_exposicion NUMBER;
+    v_visitas NUMBER; --GUARDARÁN EL NÚMERO DE VISITAS
     
 BEGIN
-    SELECT COUNT(*) INTO v_visitas FROM VISITA WHERE cod_actividad = :OLD.cod_actividad;
+    SELECT COUNT(*) INTO v_visitas FROM VISITAS WHERE cod_visita = :OLD.cod_visita;
 
     IF v_visitas > 0 THEN
-        RAISE_APPLICATION_ERROR(-20008,"No se puede borrar la actividad porque hay una visita activa");
-    END IF;
-
-    SELECT COUNT(*) INTO v_exposicion FROM EXPOSICION WHERE cod_actividad = :OLD.cod_actividad;
-
-    IF v_exposicion > 0 THEN
-        RAISE_APPLICATION_ERROR(-20009,"No se puede borrar la actividad porque hay una exposicion activa");
+        RAISE_APPLICATION_ERROR(-20008,'No se puede borrar la actividad porque hay una visita activa');
     END IF;
 END;
 /
@@ -68,7 +105,7 @@ BEGIN
 
     IF :NEW.fecha_creacion IS NOT NULL AND v_fecha IS NOT NULL THEN
         IF :NEW.fecha_creacion < v_fecha THEN
-            RAISE_APPLICATION_ERROR(-20003, "La fecha de creacion de la obra no puede ser anterior a la de nacimiento del autor");
+            RAISE_APPLICATION_ERROR(-20003, 'La fecha de creacion de la obra no puede ser anterior a la de nacimiento del autor');
         END IF;
     END IF;
 END;
